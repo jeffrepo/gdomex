4709b95ef0e26bf2960254947f620edc77f18a49
jeffrepo <jeffer.leo93@gmail.com>
2022-09-13 07:45:36 -0600

    por facturar

--- models/sale_order.py
+++ models/sale_order.py
@@ -11,16 +11,16 @@ class SaleOrder(models.Model):
 
     @api.depends('order_line.price_total', 'invoice_ids', 'invoice_count')
     def _new_total_invoice(self):
-        self.por_facturar = self.amount_total
         for order in self:
+            order.por_facturar = order.amount_total
             if order.invoice_ids:
                 total_facturas = 0
                 for factura in order.invoice_ids:
                     if factura.state == 'posted':
                         if factura.amount_total_signed > 0 and order.amount_total > 0:
-                            
-                            total_facturas += factura.amount_total_signed
-                            self.por_facturar = order.amount_total - total_facturas
 
-                if self.por_facturar == 0:
-                    self.invoice_status = 'invoiced'
+                            total_facturas += factura.amount_total_signed
+                            order.por_facturar = order.amount_total - total_facturas
+
+                if order.por_facturar == 0:
+                    order.invoice_status = 'invoiced'
