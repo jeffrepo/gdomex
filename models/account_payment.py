0bddb1dd4e2694fbaf70d8f27e693f42fccbb1c1
jefferson Silva <8306339+jeffrepo@users.noreply.github.com>
2023-05-12 15:46:38 -0600

    Cuenta transitoria

--- models/account_payment.py
+++ models/account_payment.py
@@ -9,6 +9,13 @@ class AccountPayment(models.Model):
 
     no_negociable = fields.Boolean(string='No negociable',default=False)
     nombre_impreso = fields.Char(string='Nombre impreso')
+    cuenta_transitoria_id = fields.Many2one('account.account', 'Cuenta transitoria')
+
+    @api.depends('journal_id', 'partner_id', 'partner_type', 'is_internal_transfer')
+    def _compute_destination_account_id(self):
+        super(AccountPayment, self)._compute_destination_account_id()
+        if self.cuenta_transitoria_id:
+            self.destination_account_id = self.cuenta_transitoria_id.id    
 
 class AccountMove(models.Model):
     _inherit = "account.move"
