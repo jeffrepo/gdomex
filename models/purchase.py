95132ff2148d0be9fa8db717bddf1b2564c570ed
jefferson Silva <8306339+jeffrepo@users.noreply.github.com>
2025-01-23 12:10:39 -0600

    Secuencia domex impo

--- models/purchase.py
+++ models/purchase.py
@@ -56,6 +56,15 @@ class PurchaseOrder(models.Model):
                     if 'date_order' in vals:
                         seq_date = fields.Datetime.context_timestamp(self, fields.Datetime.to_datetime(vals['date_order']))
                     vals['name'] = self_comp.env['ir.sequence'].next_by_code('purchase_order_almex_code', sequence_date=seq_date) or '/'
+                    
+            if vals['picking_type_id'] == 155:
+
+                if vals.get('name', 'New') == 'New':
+                    seq_date = None
+                    if 'date_order' in vals:
+                        seq_date = fields.Datetime.context_timestamp(self, fields.Datetime.to_datetime(vals['date_order']))
+                    vals['name'] = self_comp.env['ir.sequence'].next_by_code('purchase_order_domex_imp_code', sequence_date=seq_date) or '/'
+                    
             if vals['picking_type_id'] == 154:
 
                 if vals.get('name', 'New') == 'New':
