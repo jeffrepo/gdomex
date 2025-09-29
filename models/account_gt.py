54b88f695cac883149600947c5e944ee7a5d15b9
jefferson Silva <8306339+jeffrepo@users.noreply.github.com>
2025-09-29 12:51:07 -0600

    Cuenta analitica flete y transporte

--- models/account_gt.py
+++ models/account_gt.py
@@ -44,35 +44,7 @@ class Liquidacion(models.Model):
         result = super(Liquidacion, self).create(vals)
         return result
 
-    # def write(self, values):
-    #     seq_date = None
-    #     logging.warning('write')
-    #     company_id = self.env.company.id
-    #     logging.warning(company_id)
-    #     if company_id == 1:
-    #         vals['name'] = self.env['ir.sequence'].next_by_code(
-    #         'seq_accont_gt_liquidacion_domex', sequence_date=seq_date) or _('New')
-    #     if company_id == 6:
-    #         vals['name'] = self.env['ir.sequence'].next_by_code(
-    #         'seq_accont_gt_liquidacion_almex', sequence_date=seq_date) or _('New')
-    #     if company_id == 7:
-    #         vals['name'] = self.env['ir.sequence'].next_by_code(
-    #         'seq_accont_gt_liquidacion_recubrimientos', sequence_date=seq_date) or _('New')
-    #     if company_id == 8:
-    #         vals['name'] = self.env['ir.sequence'].next_by_code(
-    #         'seq_accont_gt_liquidacion_neira', sequence_date=seq_date) or _('New')
-    #     if company_id == 9:
-    #         logging.warning('es 9')
-    #         vals['name'] = self.env['ir.sequence'].next_by_code(
-    #         'seq_accont_gt_liquidacion_bordalas', sequence_date=seq_date) or _('New')             
-    #     if company_id == 10:
-    #         vals['name'] = self.env['ir.sequence'].next_by_code(
-    #         'seq_accont_gt_liquidacion_argentiera', sequence_date=seq_date) or _('New')                  
-    #     if company_id == 11:
-    #         vals['name'] = self.env['ir.sequence'].next_by_code(
-    #         'seq_accont_gt_liquidacion_vinyasa', sequence_date=seq_date) or _('New')                  
-    #     if company_id == 12:
-    #         vals['name'] = self.env['ir.sequence'].next_by_code(
-    #         'seq_accont_gt_liquidacion_galmex', sequence_date=seq_date) or _('New')  
-    #     result = super(Liquidacion, self).write(values)
-    #     return result
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    ft_cuenta_analitica_ids = fields.Many2many('Cuenta analitica flete y transporte')
