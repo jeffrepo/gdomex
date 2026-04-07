36f8e7d9df25d7bdb6620baa30f51e54c6631407
jefferson Silva <8306339+jeffrepo@users.noreply.github.com>
2026-04-07 09:39:17 -0600

    Refactor create method to handle multiple records

--- models/account_gt.py
+++ models/account_gt.py
@@ -4,43 +4,41 @@ import logging
 class Liquidacion(models.Model):
     _inherit = "account_gt.liquidacion"
 
-    @api.model
-    def create(self, vals):
-        
-        seq_date = None
-        logging.warning('create')
-        company_id = self.env.company.id
-        logging.warning(company_id)
-        if company_id == 1:
-            vals['name'] = self.env['ir.sequence'].next_by_code(
-            'account_gt.liquidacion_domex', sequence_date=seq_date) or _('New')
-        if company_id == 6:
-            vals['name'] = self.env['ir.sequence'].next_by_code(
-            'account_gt.liquidacion_almex', sequence_date=seq_date) or _('New')
-        if company_id == 7:
-            vals['name'] = self.env['ir.sequence'].next_by_code(
-            'account_gt.liquidacion_recubrimientos', sequence_date=seq_date) or _('New')
-        if company_id == 8:
-            vals['name'] = self.env['ir.sequence'].next_by_code(
-            'account_gt.liquidacion_neira', sequence_date=seq_date) or _('New')
-        if company_id == 9:
-            logging.warning('es 9')
-            vals['name'] = self.env['ir.sequence'].next_by_code(
-            'account_gt.liquidacion_bordalas', sequence_date=seq_date) or _('New')             
-        if company_id == 10:
-            vals['name'] = self.env['ir.sequence'].next_by_code(
-            'account_gt.liquidacion_argentiera', sequence_date=seq_date) or _('New')                  
-        if company_id == 11:
-            vals['name'] = self.env['ir.sequence'].next_by_code(
-            'account_gt.liquidacion_vinyasa', sequence_date=seq_date) or _('New')                  
-        if company_id == 12:
-            vals['name'] = self.env['ir.sequence'].next_by_code(
-            'account_gt.liquidacion_galmex', sequence_date=seq_date) or _('New')
-        if company_id == 38:
-            vals['name'] = self.env['ir.sequence'].next_by_code(
-            'account_gt.liquidacion_aply', sequence_date=seq_date) or _('New') 
-        logging.warning('vals')
-        logging.warning(vals)
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            seq_date = None
+            logging.warning('create')
+            company_id = self.env.company.id
+            logging.warning(company_id)
+            if company_id == 1:
+                vals['name'] = self.env['ir.sequence'].next_by_code(
+                'account_gt.liquidacion_domex', sequence_date=seq_date) or _('New')
+            if company_id == 6:
+                vals['name'] = self.env['ir.sequence'].next_by_code(
+                'account_gt.liquidacion_almex', sequence_date=seq_date) or _('New')
+            if company_id == 7:
+                vals['name'] = self.env['ir.sequence'].next_by_code(
+                'account_gt.liquidacion_recubrimientos', sequence_date=seq_date) or _('New')
+            if company_id == 8:
+                vals['name'] = self.env['ir.sequence'].next_by_code(
+                'account_gt.liquidacion_neira', sequence_date=seq_date) or _('New')
+            if company_id == 9:
+                logging.warning('es 9')
+                vals['name'] = self.env['ir.sequence'].next_by_code(
+                'account_gt.liquidacion_bordalas', sequence_date=seq_date) or _('New')             
+            if company_id == 10:
+                vals['name'] = self.env['ir.sequence'].next_by_code(
+                'account_gt.liquidacion_argentiera', sequence_date=seq_date) or _('New')                  
+            if company_id == 11:
+                vals['name'] = self.env['ir.sequence'].next_by_code(
+                'account_gt.liquidacion_vinyasa', sequence_date=seq_date) or _('New')                  
+            if company_id == 12:
+                vals['name'] = self.env['ir.sequence'].next_by_code(
+                'account_gt.liquidacion_galmex', sequence_date=seq_date) or _('New')
+            if company_id == 38:
+                vals['name'] = self.env['ir.sequence'].next_by_code(
+                'account_gt.liquidacion_aply', sequence_date=seq_date) or _('New')         
         result = super(Liquidacion, self).create(vals)
         return result
 
