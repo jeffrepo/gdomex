2bd5a64204068502d5bb03fb4001b96d7d96fdc1
jeffrepo <jeffer.leo93@gmail.com>
2026-04-23 10:57:25 -0600

    Costo por tipo de cambio compra

--- models/stock_move.py
+++ models/stock_move.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 from odoo.tools.float_utils import float_compare, float_is_zero, float_round
+import logging
+
+VALUATION_DICT = {
+    'value': 0,
+    'quantity': 0,
+    'description': False,
+}
+
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
@@ -20,35 +28,59 @@ class StockMove(models.Model):
         if self.product_id.uom_id.name == "m" and (self.unidad > 0 or self.largo_gdomex > 0):
             self.product_uom_qty = self.unidad * self.largo_gdomex
 
-    def _get_price_unit(self):
-        """ Returns the unit price for the move"""
+    def _get_value_from_quotation(self, quantity, at_date=None):
         self.ensure_one()
-        if not self.origin_returned_move_id and self.purchase_line_id and self.product_id.id == self.purchase_line_id.product_id.id:
-            price_unit_prec = self.env['decimal.precision'].precision_get('Product Price')
-            line = self.purchase_line_id
-            order = line.order_id
-            price_unit = line._prepare_compute_all_values()['price_unit']
-            if line.taxes_id:
-                qty = line.product_qty or 1
-                price_unit = line.taxes_id.with_context(round=False).compute_all(price_unit, currency=line.order_id.currency_id, quantity=qty, product=self.product_id)['total_void']
-                price_unit = float_round(price_unit / qty, precision_digits=price_unit_prec)
-            if line.product_uom.id != line.product_id.uom_id.id:
-                price_unit *= line.product_uom.factor / line.product_id.uom_id.factor
-            if order.currency_id != order.company_id.currency_id:
-                # The date must be today, and not the date of the move since the move move is still
-                # in assigned state. However, the move date is the scheduled date until move is
-                # done, then date of actual move processing. See:
-                # https://github.com/odoo/odoo/blob/2f789b6863407e63f90b3a2d4cc3be09815f7002/addons/stock/models/stock_move.py#L36
 
-                if order.fecha_tipo_cambio:
-                    price_unit = order.currency_id._convert(
-                        price_unit, order.company_id.currency_id, order.company_id, order.fecha_tipo_cambio, round=False)
-                    self.price_unit = price_unit
-                else:
-                    price_unit = order.currency_id._convert(
-                        price_unit, order.company_id.currency_id, order.company_id, fields.Date.context_today(self), round=False)
-            return price_unit
-        return super(StockMove, self)._get_price_unit()
+        line = self.purchase_line_id
+        if not line or self.product_id != line.product_id:
+            return super()._get_value_from_quotation(quantity, at_date=at_date)
+        order = line.order_id
+        price_unit_prec = self.env['decimal.precision'].precision_get('Product Price')
+
+        # Precio base de la línea de compra
+        price_unit = line.price_unit or 0.0
+
+        # Impuestos
+        if line.tax_ids:
+            qty_po = line.product_qty or 1.0
+            taxes_res = line.tax_ids.with_context(round=False).compute_all(
+                price_unit,
+                currency=order.currency_id,
+                quantity=qty_po,
+                product=line.product_id,
+                partner=order.partner_id,
+            )
+            # Si total_void no existe en tu rama, usa total_excluded
+            net_total = taxes_res.get('total_void', taxes_res.get('total_excluded', price_unit * qty_po))
+            price_unit = float_round(
+                net_total / qty_po,
+                precision_digits=price_unit_prec,
+            )
+
+        # UoM de la línea de compra -> UoM del producto
+        if line.product_uom_id and line.product_uom_id != line.product_id.uom_id:
+            price_unit *= line.product_uom_id.factor / line.product_id.uom_id.factor
+
+        conversion_date = False
+        if order.currency_id != order.company_id.currency_id:
+            conversion_date = order.fecha_tipo_cambio or order.date_order or fields.Date.context_today(self)
+            price_unit = order.currency_id._convert(
+                price_unit,
+                order.company_id.currency_id,
+                order.company_id,
+                conversion_date,
+                round=False,
+            )
+
+        return {
+            'value': price_unit * quantity,
+            'quantity': quantity,
+            'description': _(
+                "Valued from purchase order %(po)s%(fx)s",
+                po=order.name,
+                fx=conversion_date and _(" using exchange date %(date)s", date=conversion_date) or "",
+            ),
+        }
 
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
