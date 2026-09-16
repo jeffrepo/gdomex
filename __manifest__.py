b386fcc8009f765b34883f82cdf28f0d258d5e8b
Jefferson Silva <jeffer.leo93@gmail.com>
2026-09-16 12:10:44 -0600

    feat: agrupar informe de costos por proveedor

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
     'author': 'Silva technologies',
     'website': '',
     'category': 'Inventory',
-    'version': '19.0.1.0.6',
+    'version': '19.0.1.0.7',
     'license': 'LGPL-3',
     'depends': [
         'stock',
